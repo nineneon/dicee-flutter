ba785e84be97f2bf6d773f8255264095477cd93f
jvillanueva <jvillanueva@ninth-wave.com>
2022-06-22 02:07:53 +0800

    dicee

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int left = Random().nextInt(6)+1;
+  int right = Random().nextInt(6)+1;
+
+  void rollDice(){
+    setState(() {
+      left = Random().nextInt(6)+1;
+      right = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$left.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: rollDice,
+              child: Image.asset('images/dice$right.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
